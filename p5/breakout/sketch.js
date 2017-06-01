dc1dbf9544da1f458790991fcd33bf6baf253f58
Aditya Pokharel <pokhareladitya@gmail.com>
2017-06-01 20:10:25 +1000

    minor edits

--- p5/breakout/sketch.js
+++ p5/breakout/sketch.js
@@ -28,7 +28,7 @@ function setup() {
     }
 }
 
-
+//gets called on a loop.
 function draw() {
     background(100);
 
@@ -38,6 +38,8 @@ function draw() {
 
     for(var i = 0; i < 24; i++){
         blocks[i].display();
+
+		//if the ball hits any of the blocks.
         if((ball.x > blocks[i].x && ball.x < (blocks[i].x + block_width)) && (ball.y > blocks[i].y && ball.y < (blocks[i].y + block_height))){
             ball.hit_block();
             blocks[i].dead = true;
@@ -47,9 +49,11 @@ function draw() {
         }
     }
 
+	//if all the blocks are hit.
     if(score == 24){
         alert("Game Over, You Win!");
-        remove();
+		location.reload();
+        //remove();
     }
 
     if(ball.x <= 0 || ball.x >= CANVAS_WIDTH){
@@ -66,34 +70,39 @@ function draw() {
 
     if(ball.y > CANVAS_HEIGHT){
         alert("Game Over! You Lose!");
-        remove();
+		location.reload();
+        //remove();
     }
 
     if(ball.y < 0){
         ball.hit_ceiling();
     }
 
-	
-	
+
+
 }
 
-
+//"reflector" object that can be moved by the left and right arrow keys.
+// bounces the ball back.
 function Reflector(){
     this.x = 0;
     this.y = 388;
     this.width = 80;
     this.height = 10;
 
+	//sets the X position of the reflector
     this.set_x = function(x){
         this.x = x;
     };
 
+	//displays the reflector
     this.display = function(){
         rect(this.x, this.y, this.width, this.height);
     };
-	
+
 }
 
+//detects the key pressed
 function keyPressed(){
 	if(keyCode == LEFT_ARROW){
 		if(reflector.x > 0){
@@ -107,6 +116,7 @@ function keyPressed(){
 	}
 }
 
+// "block" object. Array of these exist at the top of the canvas.
 function Block(x, y){
     this.dead = false;
     this.x = x;
@@ -114,6 +124,7 @@ function Block(x, y){
     this.left = x;
     this.right = x + 50;
 
+	//displays the individual block.
     this.display = function(){
         if(!this.dead){
             rect(x, y, block_width, block_height);
@@ -122,6 +133,8 @@ function Block(x, y){
 
 }
 
+//"Ball" object. Bounces off if it comes in contact
+//with any surface (except the bottom of the screen)
 function Ball(){
     this.diameter = 20;
     this.x = (reflector.width /2);
@@ -130,28 +143,35 @@ function Ball(){
     this.xspeed = 1;
     this.yspeed = -1;
 
+	//displays the ball.
     this.display = function(){
         ellipse(this.x, this.y, this.diameter, this.diameter);
     };
 
+	//moves the ball.
     this.move = function () {
         this.x += this.xspeed * 3;
         this.y += this.yspeed * 3;
     };
 
+	//Random change to xspeed and yspeed if the ball hits
+	// the reflector.
     this.hit_reflector = function() {
         this.xspeed = random(-1, 1);
         this.yspeed = this.yspeed * -1;
     };
 
+	//predictive, symmetrical reflection on the x axis.
     this.hit_block = function(){
         this.yspeed = this.yspeed * -1;
     };
 
+	//predictive symmetrical reflection on the Y axis
     this.hit_wall = function(){
         this.xspeed = this.xspeed * -1;
     };
 
+	//predictive symmetrical reflection on the x axis.
     this.hit_ceiling = function(){
         this.yspeed = this.yspeed * -1;
     };
